b542a965b1ed216f4a77369db6c5032a53c8dac4
Iancu Vlad <vladi@stonesouptech.com>
2020-11-19 00:40:47 +0200

    Logging

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
     $data = json_decode(file_get_contents("php://input"), true);
+    error_log("action is".$data["action"]);
     if($data["pull_request"]["base"]["ref"] == "master" && $data["action"] == "merged") {
+        error_log("We are pulling the master branch");
         chdir("/home/iancu/apps/hotel-php-lab-backend");
         exec("git pull origin master");
     }
